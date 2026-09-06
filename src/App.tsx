ac38650107faefb0ed6ce9c491e6e63ccf8cca8a
rezasdsju <rezaul2002104@gmail.com>
2026-09-06 21:16:01 +0600

    Add SearchBar component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import SearchBar from './components/Search/SearchBar'
+function App() {
+
+  return (
+    <>
+    <SearchBar></SearchBar>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Search/SearchBar.tsx
@@ -0,0 +1,11 @@
+
+
+export default function SearchBar(){
+    return (
+        <form action="">
+            <input type="text" /> <br />
+            <input type="checkbox"  />
+            <label htmlFor="">Only show Products in Stock</label>
+        </form>
+    )
+}
