5ecfcbcd6fcb897058ee571ff90c6a945c456180
rezasdsju <rezaul2002104@gmail.com>
2026-09-06 21:22:50 +0600

    Add typs.ts file to declare types

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,17 @@ import './App.css'
 import SearchBar from './components/Search/SearchBar'
 import ProductTable from './components/ProductTable/ProductTable'
 
+import type { Product } from './type'
+
+const products: Product[] = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+]
+
 function App() {
 
   return (
