83143919c054a37745e5361619bd5a0ecc956705
rezasdsju <rezaul2002104@gmail.com>
2026-09-06 21:19:58 +0600

    Initialize ProductTable component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 
 import './App.css'
 import SearchBar from './components/Search/SearchBar'
+import ProductTable from './components/ProductTable/ProductTable'
+
 function App() {
 
   return (
     <>
     <SearchBar></SearchBar>
+    <ProductTable></ProductTable>
     </>
   )
 }
 
 export default App
+
---
+++ src/components/ProductTable/ProductTable.tsx
@@ -0,0 +1,13 @@
+
+export default function ProductTable(){
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <td>name</td>
+                    <td>Price</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
